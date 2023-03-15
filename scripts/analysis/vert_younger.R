d529e31b71256d17a51a3de46ff82e0ae77837d3
Ruby E. Stephens <stephenseruby@gmail.com>
2023-03-16 10:30:54 +1100

    gave constraining vertebrate pollination a red hot go but don't think it's possible with currently available analysis functions

--- scripts/analysis/vert_younger.R
+++ scripts/analysis/vert_younger.R
@@ -38,6 +38,26 @@ tree <- ape::read.tree("data_input/eFLOWER-1209_RC_complete_MCC.phy")
 # read in prepped pollination data
 source("scripts/pollination1209.R")
 
+# cut tree down to only vert/insect tips
+tree_vert_insect <- ape::drop.tip(tree, pollination1209$taxon_name[pollination1209$vert_insect == "?"])
 
+# reformat vert/insect data as vector for diversitree analysis
+vert_insect <- dplyr::na_if(pollination1209$vert_insect, "?")
+names(vert_insect) <- pollination1209$taxon_name
+# diversitree can't handle polymorphic data, convert these to NA
+vert_insect <- dplyr::na_if(vert_insect, "6&5")
+vert_insect <- gsub("5", "0", vert_insect)
+vert_insect <- gsub("6", "1", vert_insect)
 
+# run experimental analysis to reconstruct vert/insect pollination where
+# vertebrate pollination forbidden before K-Pg mass extinction (66 mya)
+# this function only possible with a binary character, thus can't run for
+# whole insect-vertebrate-wind-water pollination scenario
+diversitree::make.bisse.td(tree_vert_insect, states = vert_insect, n.epoch = 2,
+              nt.extra=10, strict=TRUE, control=list())
+# BUT I cannot work out looking through the documentation where to specify
+# constant rates of speciation/extinction, or where to specify the time
+# split as 66 mya, or where to specify that I want only insect pollination
+# before that time split. And, is this even wise, or will it just give strange
+# and spurious results?
 
